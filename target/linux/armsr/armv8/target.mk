516fe81ae5d34d77e7f84df64f0b43677938d93d
Stefan Hellermann <stefan@the2masters.de>
2025-01-14 14:23:36 +0100

    armsr: armv8: use compressed kernel to reduce image size
    while at it change compiler target from generic to cortex-a53 to
    be able to reuse the toolchain from openwrt-one /‌ filogic target.
    
    Not for upstream inclusion!

--- target/linux/armsr/armv8/target.mk
+++ target/linux/armsr/armv8/target.mk
@@ -1,6 +1,8 @@
 ARCH:=aarch64
 SUBTARGET:=armv8
 BOARDNAME:=64-bit (armv8) machines
+CPU_TYPE:=cortex-a53
+KERNELNAME:=vmlinuz.efi
 
 define Target/Description
   Build multi-platform images for the ARMv8 instruction set architecture
